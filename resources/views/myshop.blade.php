f130419449d49ed4feba1bd1346ff33e02d36327
Sukijtra <sukijtra.k@kkumail.com>
2024-08-15 17:36:41 +0700

    laravel itshop

---
+++ resources/views/myshop.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>เกี่ยวกับร้าน IT Shop</h1>
+    <ul>
+        <li>ที่อยู่ร้าน:IT CP KKU</li>
+        <li>เบอร์โทร:0888888888</li>
+        <li>ที่ตั้งร้าน Khon Kean</li>
+    </ul>
+</body>
+</html>
